6a684991c9d750719e2fcff7f01456260bc652f1
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-08 21:14:38 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,16 @@
+OLED.init(128, 64)
+let itme = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
+itme.DateTime(
+2025,
+2,
+8,
+6,
+21,
+11,
+22
+)
+basic.forever(function () {
+    OLED.writeNum(Math.round(itme.getSecond()))
+    basic.pause(1000)
+    OLED.clear()
+})
